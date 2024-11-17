81396b18c293bea57e2cb5d238a508117106c513
Zgirdan Evgenii <zgirdan.evg@gmail.com>
2024-11-17 20:02:21 +0300

    adawdawd

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import TelegramLoginButton from "react-telegram-login";
+
+function BotButton() {
+    const onAuth = ctx => {
+        console.log(ctx);
+    };
+
+    return <TelegramLoginButton botName="testbtnbbot" requestAccess={'7837491841:AAEB2DdjfsEU5C1Kcd9GJL0X2uKyk1wbmk8'} dataOnAuth={onAuth} buttonSize={'large'}  dataAuthUrl={'https://test-telegram-auth.vercel.app/'}/>;
+}
 
 function App() {
   const [count, setCount] = useState(0)
+
 
   return (
     <>
@@ -18,9 +28,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <BotButton/>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
