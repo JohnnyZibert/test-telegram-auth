ba4872ee41517395981ea505ec5f327c36c7bbed
Zgirdan Evgenii <zgirdan.evg@gmail.com>
2024-11-18 22:48:02 +0300

    adawdawd

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function BotButton() {
         console.log(ctx, 'awdawd');
     };
 
-    return <TelegramLoginButton botName="testbtnbbot" dataOnAuth={onAuth} buttonSize={'large'} dataAuthUrl={'/api/auth/getJwtByInitData'} />;
+    return <TelegramLoginButton botName="testbtnbbot" dataOnAuth={onAuth} buttonSize={'large'} />;
 }
 
 function App() {
